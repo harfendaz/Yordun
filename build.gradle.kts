5aaf450c2a4d3378e71e550e91c723d444fc69b8
harfendaz <ergunvural@gmail.com>
2026-09-13 17:51:01 +0200

    snapshot pingpong

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,10 +4,11 @@ buildscript {
         mavenCentral()
         maven("https://jitpack.io")
     }
+
     dependencies {
         classpath("com.android.tools.build:gradle:8.2.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
-        classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
+        classpath("com.github.recloudstream:gradle:-SNAPSHOT")
     }
 }
 
