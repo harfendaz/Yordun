47716e40d4f67321a339226950d1b9dbec22d3b4
harfendaz <ergunvural@gmail.com>
2026-09-13 18:06:51 +0200

    Update Gradle and Kotlin versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,8 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:8.2.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+        classpath("com.android.tools.build:gradle:8.7.3")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.0")
         classpath("com.github.recloudstream:gradle:-SNAPSHOT")
     }
 }
